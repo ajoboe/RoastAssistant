5a16056fd3f2ea693ac90aae20341b2b8c508feb
Andrew Jacobson <aj@arch-bombadil>
2021-04-07 20:28:31 -0600

    mostly written. broke when adding 1c support. LineChart was working CombinedChart does not

--- app/src/main/java/com/andrewkjacobson/android/roastassistant1/ui/GraphFragment.java
+++ app/src/main/java/com/andrewkjacobson/android/roastassistant1/ui/GraphFragment.java
@@ -19,13 +19,20 @@ import com.andrewkjacobson.android.roastassistant1.db.entity.CrackReadingEntity;
 import com.andrewkjacobson.android.roastassistant1.db.entity.ReadingEntity;
 import com.andrewkjacobson.android.roastassistant1.model.Reading;
 import com.andrewkjacobson.android.roastassistant1.viewmodel.RoastViewModel;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -39,8 +46,10 @@ import java.util.List;
 public class GraphFragment extends Fragment {
 
     private RoastViewModel viewModel;
-
-    private LineChart chart;
+    private CombinedChart chart;
+    private static final int TEMPERATURE_SET_INDEX = 0;
+    private static final int POWER_SET_INDEX = 1;
+    private static final int FIRST_CRACK_SET_INDEX = 2;
 
     GraphView mGraph;
     LineGraphSeries<DataPoint> mGraphSeriesTemperature;
@@ -54,7 +63,6 @@ public class GraphFragment extends Fragment {
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void onChanged(@Nullable final List<ReadingEntity> readings) {
-            // todo update the graph
             if(readings != null && !readings.isEmpty()) {
                 updateGraph(readings.get(readings.size()-1));
             }
@@ -99,17 +107,6 @@ public class GraphFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_graph, container, false);
     }
 
-
-    /**
-     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
-     * has returned, but before any saved state has been restored in to the view.
-     * This gives subclasses a chance to initialize themselves once
-     * they know their view hierarchy has been completely created.  The fragment's
-     * view hierarchy is not however attached to its parent at this point.
-     *
-     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -117,15 +114,9 @@ public class GraphFragment extends Fragment {
         //  see https://developer.android.com/topic/libraries/architecture/viewmodel-savedstate
         viewModel = new ViewModelProvider(requireActivity()).get(RoastViewModel.class); // this is all we need to do it
         initGraph();
-//        updateGraph(viewModel.getRoast().getValue().getCurrentReading()); // todo this should be taken care of by an observer
         viewModel.getReadings().observe(getViewLifecycleOwner(), readingObserver);
         viewModel.getCracks().observe(getViewLifecycleOwner(), crackObserver);
     }
-
-
-
-
-
 
     // ****************
     // PRIVATE METHODS
@@ -133,9 +124,12 @@ public class GraphFragment extends Fragment {
 
     public void initGraph() {
         chart = getView().findViewById(R.id.chart);
+        chart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.LINE, CombinedChart.DrawOrder.BAR,
+        });
 
-        // enable description text
-        chart.getDescription().setEnabled(true);
+        // disable description
+        chart.getDescription().setEnabled(false);
 
         // enable touch gestures
         chart.setTouchEnabled(true);
@@ -143,18 +137,18 @@ public class GraphFragment extends Fragment {
         // enable scaling and dragging
         chart.setDragEnabled(true);
         chart.setScaleEnabled(true);
-        chart.setDrawGridBackground(false);
+        chart.setDrawGridBackground(true);
 
         // if disabled, scaling can be done on x- and y-axis separately
         chart.setPinchZoom(true);
+//        chart.setScaleXEnabled(true);
 
         // set an alternative background color
-        chart.setBackgroundColor(Color.LTGRAY);
-
-        LineData data = new LineData();
-        data.setValueTextColor(Color.WHITE);
+        chart.setBackgroundColor(Color.DKGRAY);
 
         // add empty data
+        CombinedData data = new CombinedData();
+        data.setValueTextColor(Color.WHITE);
         chart.setData(data);
 
         // get the legend (only possible after setting data)
@@ -165,25 +159,35 @@ public class GraphFragment extends Fragment {
 //        l.setTypeface(tfLight);
         l.setTextColor(Color.WHITE);
 
-        XAxis xl = chart.getXAxis();
+        XAxis xAxis = chart.getXAxis();
 //        xl.setTypeface(tfLight);
-        xl.setTextColor(Color.WHITE);
-        xl.setDrawGridLines(false);
-        xl.setAvoidFirstLastClipping(true);
-        xl.setEnabled(true);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAvoidFirstLastClipping(true);
+//        xAxis.setValueFormatter(new ValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//                return Math.floor(value / 60) + ":" + value%60 ;
+//            }
+//        });
+        xAxis.setEnabled(true);
 
+        // axis of temperature (left)
         YAxis leftAxis = chart.getAxisLeft();
 //        leftAxis.setTypeface(tfLight);
         leftAxis.setTextColor(Color.WHITE);
-        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMaximum(viewModel.getSettings().getMaxGraphTemperature());
         leftAxis.setAxisMinimum(0f);
         leftAxis.setDrawGridLines(true);
 
+        // axis of power (right)
         YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-
-
+        rightAxis.setTextColor(Color.WHITE);
+        rightAxis.setAxisMaximum(100f);
+        rightAxis.setAxisMinimum(0f);
+        rightAxis.setGranularity(25f);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setEnabled(true);
 
 //        mGraph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
 //            @Override
@@ -236,48 +240,176 @@ public class GraphFragment extends Fragment {
 //        }
     }
 
-
-    // todo this should just get called by an observer
-    @RequiresApi(api = Build.VERSION_CODES.O)
     public void updateGraph(Reading reading) {
-        int elapsed;
-        int expectedRoastLength;
-        if(viewModel.getRoast().getValue() != null) {
-            elapsed = viewModel.getElapsed();
-            expectedRoastLength = viewModel.getSettings().getExpectedRoastLength();
+        CombinedData combinedData = chart.getData();
+
+        if(combinedData.getLineData() == null) {
+            combinedData.setData(new LineData());
+        }
+
+        if(combinedData != null) {
+            ILineDataSet temperatureSet = combinedData.getLineData().getDataSetByIndex(TEMPERATURE_SET_INDEX);
+            ILineDataSet powerSet = combinedData.getLineData().getDataSetByIndex(POWER_SET_INDEX);
+            // temperatureSet.addEntry(...); // can be called as well
+
+            // initialize data sets as needed
+            if(temperatureSet == null) {
+                temperatureSet = createTemperatureSet();
+                combinedData.getLineData().addDataSet(temperatureSet);
+//                combinedData.addDataSet(temperatureSet);
+            }
+            if(powerSet == null) {
+                powerSet = createPowerSet();
+                combinedData.getLineData().addDataSet(powerSet);
+//                combinedData.addDataSet(powerSet);
+            }
+
+            // add temperature and power
+            combinedData.getLineData().addEntry(new Entry(
+                    reading.getSeconds(), reading.getTemperature()), TEMPERATURE_SET_INDEX);
+            combinedData.getLineData().addEntry(new Entry(
+                    reading.getSeconds(), reading.getPower()), POWER_SET_INDEX);
+
+            combinedData.getLineData().notifyDataChanged();
+            combinedData.notifyDataChanged();
+            chart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            chart.setVisibleXRangeMinimum(0);
+            chart.setVisibleXRangeMaximum(viewModel.getSettings().getExpectedRoastLength());
+            chart.setVisibleYRange(50, viewModel.getSettings().getMaxGraphTemperature(), YAxis.AxisDependency.LEFT);
+            chart.setVisibleYRange(0, 100, YAxis.AxisDependency.RIGHT);
+
+            // move to the latest entry
+            chart.moveViewToX(combinedData.getEntryCount());
+            chart.invalidate();
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f, AxisDependency.LEFT);
+
         } else {
-            elapsed = 0;
-            expectedRoastLength = 0;
+            throw new RuntimeException("chartData was null");
         }
-        try {
-//            mGraphSeriesTemperature.
-            mGraphSeriesTemperature.appendData(new DataPoint(
-                            reading.getSeconds(),
-                            reading.getTemperature()),
-                    false, 99999);
-            mGraphSeriesPower.appendData(new DataPoint(
-                            reading.getSeconds(),
-                            reading.getPower()),
-                    false, 99999); // if scrollToEnd is true, shows negatives in the beginning
-            if(elapsed > expectedRoastLength) mGraph.getViewport().scrollToEnd();
-        } catch (IllegalArgumentException e) {
-            System.err.println("failed to update graph.");
-        }
+// END MPCHART-----
+//
+//        int elapsed;
+//        int expectedRoastLength;
+//        if(viewModel.getRoast().getValue() != null) {
+//            elapsed = viewModel.getElapsed();
+//            expectedRoastLength = viewModel.getSettings().getExpectedRoastLength();
+//        } else {
+//            elapsed = 0;
+//            expectedRoastLength = 0;
+//        }
+//        try {
+////            mGraphSeriesTemperature.
+//            mGraphSeriesTemperature.appendData(new DataPoint(
+//                            reading.getSeconds(),
+//                            reading.getTemperature()),
+//                    false, 99999);
+//            mGraphSeriesPower.appendData(new DataPoint(
+//                            reading.getSeconds(),
+//                            reading.getPower()),
+//                    false, 99999); // if scrollToEnd is true, shows negatives in the beginning
+//            if(elapsed > expectedRoastLength) mGraph.getViewport().scrollToEnd();
+//        } catch (IllegalArgumentException e) {
+//            System.err.println("failed to update graph.");
+//        }
+    }
+
+    private LineDataSet createGeneralSet() {
+        LineDataSet set = new LineDataSet(null, "");
+        set.setLineWidth(2f);
+        set.setCircleRadius(1f);
+        set.setCircleColor(Color.WHITE);
+        set.setFillAlpha(65);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+
+        return set;
+    }
+
+    private LineDataSet createTemperatureSet() {
+        LineDataSet set = createGeneralSet();
+        set.setLabel("Temperature");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setDrawValues(true);
+        return set;
+    }
+
+    private LineDataSet createPowerSet() {
+        LineDataSet set = createGeneralSet();
+        set.setLabel("Power");
+        set.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        set.setColor(ColorTemplate.rgb("#FF0000"));
+        set.setDrawValues(false);
+        return set;
+    }
+
+    private BarDataSet createFirstCrackSet() {
+        BarDataSet set = new BarDataSet(null, "First Crack");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.rgb("#00FF00"));
+        // todo setBarWidth(4f) width on the BarData
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(12f);
+        set.setDrawValues(true);
+        return set;
     }
 
     private void graphFirstCrack(List<CrackReadingEntity> crackReadingEntities) {
-        CrackReadingEntity firstCrack = null;
-        for(CrackReadingEntity c : crackReadingEntities) { // find the last 1C
-            if(c.getCrackNumber() == 1) firstCrack = c;
-        }
-        if(firstCrack != null && firstCrack.hasOccurred()) {
-            // todo add time and temp labels
-            BarGraphSeries<DataPoint> series = new BarGraphSeries();
-            series.appendData(new DataPoint(
-                    firstCrack.getSeconds(), mMaxGraphTemperature), false, 99);
-            series.setDataWidth(5);
-            series.setColor(Color.GREEN);
-            mGraph.addSeries(series);
+        BarData chartData = chart.getData().getBarData();
+
+        if(chartData != null) {
+            IBarDataSet firstCrackSet = chartData.getDataSetByIndex(FIRST_CRACK_SET_INDEX);
+            firstCrackSet.setDrawValues(true);
+            // temperatureSet.addEntry(...); // can be called as well
+
+            // initialize data sets as needed
+            if(firstCrackSet == null) {
+                firstCrackSet = createFirstCrackSet();
+                chartData.addDataSet(firstCrackSet);
+            }
+
+            // add first crack entry
+            CrackReadingEntity firstCrack = null;
+            for(CrackReadingEntity c : crackReadingEntities) { // find the last 1C
+                if(c.getCrackNumber() == 1) firstCrack = c;
+            }
+            if(firstCrack != null && firstCrack.hasOccurred()) {
+
+
+
+
+
+                // todo add time and temp labels
+                chartData.addEntry(new Entry(firstCrack.getSeconds(),
+                        firstCrack.getTemperature()), FIRST_CRACK_SET_INDEX);
+                chartData.notifyDataChanged();
+                chart.notifyDataSetChanged();
+                // limit the number of visible entries
+//            chart.setVisibleXRangeMinimum(0);
+//            chart.setVisibleXRangeMaximum(viewModel.getSettings().getExpectedRoastLength());
+//            chart.setVisibleYRange(50, viewModel.getSettings().getMaxGraphTemperature(), YAxis.AxisDependency.LEFT);
+//            chart.setVisibleYRange(0, 100, YAxis.AxisDependency.RIGHT);
+            }
+            // END MPChart--------
+//
+//
+//        CrackReadingEntity firstCrack = null;
+//        for(CrackReadingEntity c : crackReadingEntities) { // find the last 1C
+//            if(c.getCrackNumber() == 1) firstCrack = c;
+//        }
+//        if(firstCrack != null && firstCrack.hasOccurred()) {
+//            BarGraphSeries<DataPoint> series = new BarGraphSeries();
+//            series.appendData(new DataPoint(
+//                    firstCrack.getSeconds(), mMaxGraphTemperature), false, 99);
+//            series.setDataWidth(5);
+//            series.setColor(Color.GREEN);
+//            mGraph.addSeries(series);
 //            double firstCrackLineLeftX = mGraph.getSeries().get(mGraph.getSeries().indexOf(series)).getLowestValueX();
 //
 //            Canvas canvas = new Canvas();
