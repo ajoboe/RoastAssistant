c890e7f6d6fa75b319e4aef9c100f0122b05bca1
Andrew Jacobson <aj@bombadil>
2021-06-23 09:10:08 -0600

    test ReadingDao get()

--- app/src/androidTest/java/com/andrewkjacobson/android/roastassistant/db/dao/ReadingDaoTest.java
+++ app/src/androidTest/java/com/andrewkjacobson/android/roastassistant/db/dao/ReadingDaoTest.java
@@ -1,6 +1,7 @@
 package com.andrewkjacobson.android.roastassistant.db.dao;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.LiveData;
@@ -13,6 +14,7 @@ import com.jraska.livedata.TestObserver;
 
 import junit.framework.TestCase;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,56 +28,79 @@ import static org.mockito.Mockito.mock;
 @RunWith(AndroidJUnit4.class)
 public class ReadingDaoTest extends TestCase {
     @Rule
-        public InstantTaskExecutorRule testRule = new InstantTaskExecutorRule();
+    public InstantTaskExecutorRule testRule = new InstantTaskExecutorRule();
 
     private RoastViewModel viewModel;
     private Application application;
     private ReadingDao readingDao;
+    private List<ReadingEntity> expected;
 
     @Before
     public void setUp() {
         application = mock(Application.class);
-//        viewModel = new RoastViewModel(application, null);
 
         RoastRoomDatabase db = RoastRoomDatabase.getDatabase(application);
         readingDao = db.readingDao();
-
-    }
-
-//    @Test
-//    public void testGet() {
-//        fail("NOT IMPLEMENTED...");
-//    }
-
-    @Test
-    public void testGetAll() {
-//        fail("NOT IMPLEMENTED...");
+        expected = new ArrayList<ReadingEntity>();
 
         ReadingEntity r = new ReadingEntity(3, 200, 75, 999);
-//        viewModel.recordTemperature("100");
-        readingDao.insert(r);
-        LiveData<List<ReadingEntity>> readings = readingDao.getAll(999);
-//        LiveData<List<ReadingEntity>> readings = viewModel.getReadingsLiveData();
-
-        List expected = new ArrayList<ReadingEntity>();
         expected.add(r);
+        readingDao.insert(r);
+        r = new ReadingEntity(9, 230, 50, 999);
+        expected.add(r);
+        readingDao.insert(r);
+    }
+
+    @After
+    public void tearDown() {
+        readingDao.deleteAll();
+    }
+
+    @Test
+    public void testGet() {
+        int roastId = expected.get(0).getRoastId();
+        int seconds = expected.get(0).getSeconds();
+        LiveData<ReadingEntity> retrieved = readingDao.get(roastId, seconds);
 
         try {
-            assertEquals(r,
-                TestObserver.test(readings)
-                    .awaitValue()
-                    .assertHasValue()
-                    .value()
-                    .get(0));
-
+            TestObserver.test(retrieved)
+                        .awaitValue()
+                        .assertValue(expected.get(0));
         } catch (InterruptedException e) {
             e.printStackTrace();
             fail("Interrupted Exception");
         }
     }
 
-//    @Test
-//    public void testDeleteAll() {
-//        fail("NOT IMPLEMENTED...");
-//    }
+    @Test
+    public void testGetAll() {
+        LiveData<List<ReadingEntity>> retrieved = readingDao.getAll(999);
+
+        try {
+            TestObserver.test(retrieved)
+                            .awaitValue()
+                            .assertValue(expected);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail("Interrupted Exception");
+        }
+    }
+
+    @Test
+    public void testDeleteAll() {
+        readingDao.deleteAll();
+        LiveData<List<ReadingEntity>> readings = readingDao.getAll(999);
+
+        try {
+            assertTrue(TestObserver.test(readings)
+                    .awaitValue()
+                    .assertHasValue()
+                    .value()
+                    .isEmpty()
+            );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail("Interrupted Exception");
+        }
+    }
 }
