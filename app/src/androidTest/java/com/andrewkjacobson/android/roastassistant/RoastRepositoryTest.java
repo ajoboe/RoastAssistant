e85a686541604f3880582fff7754bf409aeacb09
Andrew Jacobson <aj@bombadil>
2021-06-28 11:04:11 -0600

    added failing test cases

---
+++ app/src/androidTest/java/com/andrewkjacobson/android/roastassistant/RoastRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.andrewkjacobson.android.roastassistant;
+
+import junit.framework.TestCase;
+
+public class RoastRepositoryTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetRoastLiveData() {
+        fail();
+    }
+
+    public void testGetDetailsLiveData() {
+        fail();
+    }
+
+    public void testGetReadingsLiveData() {
+        fail();
+    }
+
+    public void testGetCracksLiveData() {
+        fail();
+    }
+
+    public void testGetMostRecentRoast() {
+        fail();
+    }
+
+    public void testInsert() {
+        fail();
+    }
+
+    public void testUpdate() {
+        fail();
+    }
+
+    public void testDelete() {
+        fail();
+    }
+
+    public void testDeleteAllReadings() {
+        fail();
+    }
+}
